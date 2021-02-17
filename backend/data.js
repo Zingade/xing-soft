22a3a914c166893ca7133e36657d537f429c0420
JE Zingade <jna.zingade@gmail.com>
2021-02-17 18:16:55 +0530

    Adding files to git

---
+++ backend/data.js
@@ -0,0 +1,36 @@
+var products =  [
+    {
+        _id:"1",
+        name:"Raajma",
+        image:"/images/Raajma.jpg",
+        price:120,
+        catogory: "Grocery",
+        qty_measured_in:"Kg"
+    },
+    {
+        _id:"2",
+        name:"Matki",
+        image:"/images/Matki.jpg",
+        price:70,
+        catogory: "Grocery",
+        qty_measured_in:"Kg"
+    },
+    {
+        _id:"3",
+        name:"Kabuli Chana",
+        image:"/images/Kabuli-Chana.jpg",
+        price:110,
+        catogory: "Grocery",
+        qty_measured_in:"Kg"
+    },
+    {
+        _id:"4",
+        name:"Sona Masuri Rice",
+        image:"/images/Sona-Masoori.jpg",
+        price:45,
+        catogory: "Grocery",
+        qty_measured_in:"Kg"
+    },
+]
+
+module.exports = { products };
---
+++ frontend/src/Screens/HomeScreen.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function HomeScreen(props){
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=>{
+            const fetchData = async () => {
+                const {data} = await axios.get('/api/products');
+                setProducts(data);
+            }
+            fetchData();
+        return() => {
+
+        };
+    }, [])
+
+    return <ul className="products">
+    {
+        products.map(product =>
+        <li key={product._id}> 
+            <div className="product">
+                <img className="product-image" src={product.image} alt="products"></img>
+                <div className="product-name">{product.name}</div>
+                <div className="product-qty">Qty: <select className="product-qty">
+                    <option className="product-qty">0.5</option>
+                    <option className="product-qty">1</option>
+                    <option className="product-qty">2</option>
+                    <option className="product-qty">3</option>
+                    <option className="product-qty">4</option>
+                    <option className="product-qty">5</option>
+                    </select> {product.qty_measured_in}
+                </div>
+                <div className="product-price">₹{product.price}</div>
+                <button className="button-add-to-cart">Add to Cart</button>
+            </div>
+        </li>)
+    }
+</ul>
+
+}
+
+export default HomeScreen;
