81af38f5f5c577c6797017d4b725e36aae4fca13
JE Zingade <jna.zingade@gmail.com>
2021-02-17 19:27:03 +0530

    Updating files

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const data = require('./data');
+const path = require('path')
+
+
+const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')))
 
 app.get('/api/products', (req, res) => {
     res.send(data.products);
 })
 
-app.listen(5000, () => {console.log('Server started at http://localhost:5000')})
+app.listen(port, () => {console.log('Server started at http://localhost:'$(port))})
