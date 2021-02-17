4a37c5bc96e2332a02db89eeb4b290ab67efbfd9
JE Zingade <jna.zingade@gmail.com>
2021-02-17 19:58:23 +0530

    Updaing files

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,6 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../frontend/build')))
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')))
 
 app.get('/api/products', (req, res) => {
     res.send(data.products);
